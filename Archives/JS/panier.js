1167756b4250f3eb1ea75ff3f5bbe1b317e03fda
Sraaday <60046022+Sraaday@users.noreply.github.com>
2020-08-07 17:15:11 +0200

    Lentilles correctement affichées (panier.js)

--- Archives/JS/panier.js
+++ Archives/JS/panier.js
@@ -5,7 +5,7 @@ const url = "http://localhost:3000/api/cameras";
 function displayNbProduct() {
     let NbItems = JSON.parse(localStorage.getItem('cart')).length;
     console.log(NbItems)
-
+    
 }
 
 // Recupération des infos en local storage
@@ -19,44 +19,44 @@ infosCart();
 
 displayNbProduct();
 
-  // Récupère toutes les caméras
-  const getItems = async (url) => {
+// Récupère toutes les caméras
+const getItems = async (url) => {
     const response = await fetch(url);
     return await response.json();
-  };
+};
 
-  
+
 
 // Affiche tous les produits
 const displaycart = async () => {
     const cart = await getItems(url);
     let storedItems = JSON.parse(localStorage.getItem('cart'));
-    cart.forEach((cart) => {
-        storedItems.forEach((storedItems) => {
-        if (cart._id == storedItems.id) {
-            console.log( renderCart(cart.name, cart._id, cart.imageUrl, cart.price)) ;
-
-
-        };
+    cart.forEach((cartItem) => {
+        storedItems.forEach((storedItem) => {
+            if (cartItem._id == storedItem.id) {
+                console.log( renderCart(cartItem.name, cartItem._id, cartItem.imageUrl, cartItem.price, storedItem.lense)) ;
+            }
+            
+        });
     });
-});
 };
 
-  displaycart();
+displaycart();
 
 
 
- // Paramètres d'afichage d'un produit
-function renderCart(cartName, cartId, cartImg, cartPrice) {
+// Paramètres d'afichage d'un produit
+function renderCart(cartName, cartId, cartImg, cartPrice, cartLense) {
     const products = document.querySelector("#Produits"); 
     const article = document.createElement("article");
     article.classList.add('display-article');
     article.innerHTML = `<img alt="${cartName}" src="${cartImg}">
-      <button class="product-link" type="button"><a href="produits.html?id=${cartId}">Voir le produit</a></button>
-      <p class="product-title">${cartName}</p>
-      <p class="price">${cartPrice + " €"}</p>
-      `;
+    <button class="product-link" type="button"><a href="produits.html?id=${cartId}">Consulter la fiche du produit</a></button>
+    <p class="product-title">${cartName}</p>
+    <p class="prdoduct-lense">${cartLense}</p>
+    <p class="price">${cartPrice + " €"}</p>
+    `;
     products.appendChild(article);
-  }
+}
 
 
