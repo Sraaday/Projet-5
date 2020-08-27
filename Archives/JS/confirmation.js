2d03a21ef0ac8421353035112268c2ab8a4190e1
Sraaday <60046022+Sraaday@users.noreply.github.com>
2020-08-27 16:58:50 +0200

    CSS + réindentation

--- Archives/JS/confirmation.js
+++ Archives/JS/confirmation.js
@@ -1,26 +1,26 @@
 try{
     function renderConfirmation() {
-           // Recupération des infos de commande en local storage
-           let storedInfos = JSON.parse(localStorage.getItem('customerInfos'));
-           let storedPrice = JSON.parse(localStorage.getItem('FinalPrice'));
-           console.log(storedInfos)
-           console.log(storedPrice)
+        // Recupération des infos de commande en local storage
+        let storedInfos = JSON.parse(localStorage.getItem('customerInfos'));
+        let storedPrice = JSON.parse(localStorage.getItem('FinalPrice'));
+        console.log(storedInfos)
+        console.log(storedPrice)
         const target = document.querySelector("#Confirmation");
         const confirmation = document.createElement("article");
         confirmation.classList.add('display-article');
         
         confirmation.innerHTML = `<h1>Merci pour votre achat ${storedInfos[0].name} </h1>
-                                    <p> Le prix de votre commande s'élève à ${storedPrice[0].prixTotal} € </p>
-                                    <p> Votre numéro de commande est le suivant : ${storedInfos[0].id} </p>
-                                    <p> Nous vous remercions de votre fidélité. A très bientôt sur Orinoco ! </p> `
-
-
-
+        <p> Le prix de votre commande s'élève à ${storedPrice[0].prixTotal} € </p>
+        <p> Votre numéro de commande est le suivant : ${storedInfos[0].id} </p>
+        <p> Nous vous remercions de votre fidélité. A très bientôt sur Orinoco ! </p> `
+        
+        
+        
         target.appendChild(confirmation);
-    };
+    }
 }
 catch (error) {
     alert("Impossible de récupérer vos informations de commande !");
 }
 
-    renderConfirmation();
+renderConfirmation();
--- Archives/JS/panier.js
+++ Archives/JS/panier.js
@@ -42,8 +42,8 @@ const displaycart = async () => {
         }
     }
     
-   
-  renderCart(cart);
+    
+    renderCart(cart);
 };
 
 displaycart();
@@ -60,43 +60,43 @@ function renderCart(cart) {
     
     panier.innerHTML = `
     <div class ="col-12 .container">
-        <div class ="col-9 col-lg-9 col-sm-9 col-xs-9 col-md-9 case">
-            <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> Illustration   </div>
-            <div class ="col-4 col-lg-4 col-sm-4 col-xs-4 col-md-4 case cell"> Modèle </div>
-            <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> Lentille </div>
-            <div class ="col-1 col-lg-1 col-sm-1 col-xs-1 col-md-1 case cell"> Prix </div>
-        </div>
+    <div class ="col-9 col-lg-9 col-sm-9 col-xs-9 col-md-9 case">
+    <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> Image   </div>
+    <div class ="col-4 col-lg-4 col-sm-4 col-xs-4 col-md-4 case cell"> Modèle </div>
+    <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> Lentille </div>
+    <div class ="col-1 col-lg-1 col-sm-1 col-xs-1 col-md-1 case cell"> Prix </div>
+    </div>
     
     `;
     let totalPrice = 0;
-
+    
     for(let i = 0; i < cart.length; i++) {
         panier.innerHTML += `
         <div class ="col-9 col-lg-9 col-sm-9 col-xs-9 col-md-9 case">
-            <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> <img class="cart-img" src="${cart[i].imageUrl}"> </div>
-            <div class ="col-4 col-lg-4 col-sm-4 col-xs-4 col-md-4 case cell"> ${cart[i].name} </div>
-            <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> ${cart[i].lense} </div>
-            <div class ="col-1 col-lg-1 col-sm-1 col-xs-1 col-md-1 case cell"> ${cart[i].price} </div>
+        <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> <img class="cart-img" src="${cart[i].imageUrl}"> </div>
+        <div class ="col-4 col-lg-4 col-sm-4 col-xs-4 col-md-4 case cell"> ${cart[i].name} </div>
+        <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> ${cart[i].lense} </div>
+        <div class ="col-1 col-lg-1 col-sm-1 col-xs-1 col-md-1 case cell"> ${cart[i].price/100} </div>
         </div> 
         `
         totalPrice += cart[i].price
     }
     panier.innerHTML += `
     <div class ="row col-3">
-        <div class ="col-3 case cell"> Prix Total </div>
-        <div class ="col-3 case cell"> ${totalPrice} </div>
+    <div class ="col-3 case cell"> Prix Total </div>
+    <div class ="col-3 case cell"> ${totalPrice/100} </div>
     
-        </div>
+    </div>
     </div>
     
     `
     
     getArrayId();
     products.appendChild(panier);
-
+    
     //envoi du prix total en session Storage
     const infosPrice = {
-        prixTotal : totalPrice
+        prixTotal : totalPrice/100
     }
     let FinalPrice = JSON.parse(sessionStorage.getItem('FinalPrice'));
     
@@ -109,7 +109,7 @@ function renderCart(cart) {
     console.log(FinalPrice)
 }
 
-    
+
 
 
 // Formulaire //
@@ -117,9 +117,9 @@ function renderCart(cart) {
 function getArrayId () {
     let storedItems = JSON.parse(localStorage.getItem('cart'));
     let arrayId = [];
-
+    
     storedItems.forEach(storedItems => {
-
+        
         arrayId.push(storedItems.id)
         
     });
@@ -131,9 +131,9 @@ function getArrayId () {
 function isValid(value) {
     return /[A-Za-z\-\s]/.test(value);
 }
-    
 
-    var form = document.getElementById("form");
+
+var form = document.getElementById("form");
 form.addEventListener("submit", async  (e) => {
     e.preventDefault();
     let lama = document.querySelector("#form");
@@ -143,72 +143,72 @@ form.addEventListener("submit", async  (e) => {
     var City = form.elements.city.value;
     var Email = form.elements.email.value;
     var Ids = getArrayId();
-
+    
     if (isValid(firstName))
-        if (isValid(lastName))
-            if (isValid(City)) {
-                
-                    var contact = { "firstName" : firstName, "lastName" : lastName, "address" : address, "city" : City, "email" : Email };
-                    var req = {"contact" : contact, "products" : Ids};
-                    let options = {
-                        method:"POST",
-                        body: JSON.stringify(req),
-                        headers:{
-                            "Content-Type" : "application/json"
-                        }
-                    };
-                
-                
-                try {
-                const response = await fetch("http://localhost:3000/api/cameras/order",options);
-                const data = await response.json();
-                console.log(data)
-                
-
-                
-                
-
-
-
-                // envoi des infos en session storage
-                const OrderId = data.orderId ;
-                const infosPrice = {
-                    id : OrderId, 
-                    name : form.elements.firstName.value
-                }
-                let customerInfos = JSON.parse(sessionStorage.getItem('customerInfos'));
-                
-                if (customerInfos===null) {
-                    customerInfos = [] 
-                    
-                }
-                customerInfos.push(infosPrice);
-                localStorage.setItem('customerInfos', JSON.stringify(customerInfos));
-                console.log(customerInfos)
-            
-                
-                //redirection vers confirmation.html
-                
-                const customUrl = `confirmation.html?id=${OrderId}`;
-            
-                
-                document.location.href = customUrl;
-            }
-
-                catch (error) {
-                    alert("Error: " + error.description );
-                }
-
-            
-
-            }
-
-
+    if (isValid(lastName))
+    if (isValid(City)) {
         
- 
-
-
+        var contact = { "firstName" : firstName, "lastName" : lastName, "address" : address, "city" : City, "email" : Email };
+        var req = {"contact" : contact, "products" : Ids};
+        let options = {
+            method:"POST",
+            body: JSON.stringify(req),
+            headers:{
+                "Content-Type" : "application/json"
+            }
+        };
         
-    })
+        
+        try {
+            const response = await fetch("http://localhost:3000/api/cameras/order",options);
+            const data = await response.json();
+            console.log(data)
+            
+            
+            
+            
+            
+            
+            
+            // envoi des infos en session storage
+            const OrderId = data.orderId ;
+            const infosPrice = {
+                id : OrderId, 
+                name : form.elements.firstName.value
+            }
+            let customerInfos = JSON.parse(sessionStorage.getItem('customerInfos'));
+            
+            if (customerInfos===null) {
+                customerInfos = [] 
+                
+            }
+            customerInfos.push(infosPrice);
+            localStorage.setItem('customerInfos', JSON.stringify(customerInfos));
+            console.log(customerInfos)
+            
+            
+            //redirection vers confirmation.html
+            
+            const customUrl = `confirmation.html?id=${OrderId}`;
+            
+            
+            document.location.href = customUrl;
+        }
+        
+        catch (error) {
+            alert("Error: " + error.description );
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+})
 
 
