f6367b9666b532d5b8b967cb61a484f806c2811b
Sraaday <60046022+Sraaday@users.noreply.github.com>
2020-09-29 09:48:47 +0200

    Supression session storage, ajout url search param

--- Archives/JS/confirmation.js
+++ Archives/JS/confirmation.js
@@ -1,14 +1,18 @@
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+   
     function renderConfirmation() {
         // Recupération des infos de commande en local storage
-        let storedInfos = JSON.parse(localStorage.getItem('customerInfos'));
+        const id = urlParams.get('id')
+        const name = urlParams.get('name')
         let storedPrice = JSON.parse(localStorage.getItem('finalPrice'));
         const target = document.querySelector("#Confirmation");
         const confirmation = document.createElement("article");
         confirmation.classList.add('display-article');
         
-        confirmation.innerHTML = `<h1>Merci pour votre achat ${storedInfos[0].name} </h1>
+        confirmation.innerHTML = `<h1>Merci pour votre achat ${name} </h1>
         <p> Le prix de votre commande s'élève à ${storedPrice[0].prixTotal} € </p>
-        <p> Votre numéro de commande est le suivant : ${storedInfos[0].id} </p>
+        <p> Votre numéro de commande est le suivant : ${id} </p>
         <p> Nous vous remercions de votre fidélité. A très bientôt sur Orinoco ! </p> `
         
         
--- Archives/JS/panier.js
+++ Archives/JS/panier.js
@@ -2,6 +2,12 @@
 // API
 const url = "http://localhost:3000/api/cameras";
 
+// requête API selon L'id
+const getOneCam = async (url, id) => {
+    const response = await fetch(`${url}/${id}`);
+    return await response.json();
+};
+
 // Affichage nb produits panier
 function displayNbProduct() {
     let NbItems = JSON.parse(localStorage.getItem('cart')).length;
@@ -33,42 +39,70 @@ const displaycart = async () => {
     const cameras = await getItems(url);
     let storedItems = JSON.parse(localStorage.getItem('cart'));
     let cart = [];
-    let item;
-    let lenseId = [];
-    for(let i = 0; i < storedItems.length; i++)
-    {
+    let lenseId = [];   
+    const products = document.querySelector("#Produits");
+    const panier = document.createElement("div");
+    panier.classList.add('row');
+    panier.classList.add('render-cart');
+    
+    panier.innerHTML = `
+    <div class ="col-12 .container">
+    <div class ="col-9 col-lg-9 col-sm-9 col-xs-9 col-md-9 case">
+    <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> Image   </div>
+    <div class ="col-4 col-lg-4 col-sm-4 col-xs-4 col-md-4 case cell"> Modèle </div>
+    <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> Lentille </div>
+    <div class ="col-1 col-lg-1 col-sm-1 col-xs-1 col-md-1 case cell"> Prix </div>
+    </div>
+    
+    `;
+    let totalPrice = 0;
+    for (const item of storedItems) {
+        console.log(item)
+        let id = item.id
+        console.log(id)
         
-        for(let j = 0; j < cameras.length; j++)
-        {
-            item = null;
-            
-            if(storedItems[i].id == cameras[j]._id)
-            {
-                for(let k = 0; k < cameras[j].lenses.length; k++)
-                {
-                    
-                    if (storedItems[i].lense == cameras[j].lenses[k])
-                    {
-                        lenseId.push(cameras[j].lenses[k]);
-                    }
-                }
-                item = cameras[j];
-            }
-            
-            
-            if(item)
-            {
-                
-                item.lense = storedItems[i].lense;
-                cart.push(item);
-                
-                
-            }
-        }
+        const product = await getOneCam(url,id);
+        console.log(product)       
+        
+        panier.innerHTML += `
+        <div class ="col-9 col-lg-9 col-sm-9 col-xs-9 col-md-9 case">
+        <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> <img class="cart-img" src="${product.imageUrl}"> </div>
+        <div class ="col-4 col-lg-4 col-sm-4 col-xs-4 col-md-4 case cell"> ${product.name} </div>
+        <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> ${item.lense} </div>
+        <div class ="col-1 col-lg-1 col-sm-1 col-xs-1 col-md-1 case cell"> ${product.price/100} </div>
+        </div> 
+        `
+        totalPrice += product.price 
+        
+        
     }
     
+    panier.innerHTML += `
+    <div class ="row col-3">
+    <div class ="col-3 case cell price"> Prix Total </div>
+    <div class ="col-3 case cell price"> ${totalPrice/100} € </div>
     
-    renderCart(cart, lenseId);
+    </div>
+    </div>
+    
+    `
+    
+    infosCart();
+    products.appendChild(panier);
+    
+    //envoi du prix total en session Storage
+    const infosPrice = {
+        prixTotal : totalPrice/100
+    }
+    let finalPrice = JSON.parse(sessionStorage.getItem('finalPrice'));
+    
+    if (finalPrice===null) {
+        finalPrice = [] 
+        
+    }
+    finalPrice.push(infosPrice);
+    localStorage.setItem('finalPrice', JSON.stringify(finalPrice));
+    
 };
 
 displaycart();
@@ -186,32 +220,12 @@ form.addEventListener("submit", async  (e) => {
         try {
             const response = await fetch("http://localhost:3000/api/cameras/order",options);
             const data = await response.json();
-            
-            
-            
-            
-            
-            
-            
-            // envoi des infos en session storage
+              
+            //redirection vers confirmation.html avec des paramètres
             const OrderId = data.orderId ;
-            const infosPrice = {
-                id : OrderId, 
-                name : form.elements.firstName.value
-            }
-            let customerInfos = JSON.parse(sessionStorage.getItem('customerInfos'));
+            const name = form.elements.firstName.value;
             
-            if (customerInfos===null) {
-                customerInfos = [] 
-                
-            }
-            customerInfos.push(infosPrice);
-            localStorage.setItem('customerInfos', JSON.stringify(customerInfos));
-            
-            
-            //redirection vers confirmation.html
-            
-            const customUrl = `confirmation.html?id=${OrderId}`;
+            const customUrl = `confirmation.html?id=${OrderId}&name=${name}`;
             
             
             document.location.href = customUrl;
