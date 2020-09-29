3edddacdbe1f122638259e7873d8901bb335625e
Sraaday <60046022+Sraaday@users.noreply.github.com>
2020-09-29 10:07:54 +0200

    Supression code superflue

--- Archives/JS/panier.js
+++ Archives/JS/panier.js
@@ -1,4 +1,3 @@
-
 // API
 const url = "http://localhost:3000/api/cameras";
 
@@ -22,8 +21,6 @@ function infosCart() {
 }
 
 infosCart();
-
-
 displayNbProduct();
 
 // Récupère toutes les caméras
@@ -32,14 +29,10 @@ const getItems = async (url) => {
     return await response.json();
 };
 
-
-
 // Affiche tous les produits
 const displaycart = async () => {
     const cameras = await getItems(url);
-    let storedItems = JSON.parse(localStorage.getItem('cart'));
-    let cart = [];
-    let lenseId = [];   
+    let storedItems = JSON.parse(localStorage.getItem('cart'));  
     const products = document.querySelector("#Produits");
     const panier = document.createElement("div");
     panier.classList.add('row');
@@ -107,70 +100,6 @@ const displaycart = async () => {
 
 displaycart();
 
-
-
-// Paramètres d'afichage du panier
-function renderCart(cart, lense) {
-    
-    const products = document.querySelector("#Produits");
-    const panier = document.createElement("div");
-    panier.classList.add('row');
-    panier.classList.add('render-cart');
-    
-    panier.innerHTML = `
-    <div class ="col-12 .container">
-    <div class ="col-9 col-lg-9 col-sm-9 col-xs-9 col-md-9 case">
-    <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> Image   </div>
-    <div class ="col-4 col-lg-4 col-sm-4 col-xs-4 col-md-4 case cell"> Modèle </div>
-    <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> Lentille </div>
-    <div class ="col-1 col-lg-1 col-sm-1 col-xs-1 col-md-1 case cell"> Prix </div>
-    </div>
-    
-    `;
-    let totalPrice = 0;
-    
-    for(let i = 0; i < cart.length; i++) {
-        panier.innerHTML += `
-        <div class ="col-9 col-lg-9 col-sm-9 col-xs-9 col-md-9 case">
-        <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> <img class="cart-img" src="${cart[i].imageUrl}"> </div>
-        <div class ="col-4 col-lg-4 col-sm-4 col-xs-4 col-md-4 case cell"> ${cart[i].name} </div>
-        <div class ="col-2 col-lg-2 col-sm-2 col-xs-2 col-md-2 case cell"> ${lense[i]} </div>
-        <div class ="col-1 col-lg-1 col-sm-1 col-xs-1 col-md-1 case cell"> ${cart[i].price/100} </div>
-        </div> 
-        `
-        totalPrice += cart[i].price
-        
-    }
-    panier.innerHTML += `
-    <div class ="row col-3">
-    <div class ="col-3 case cell price"> Prix Total </div>
-    <div class ="col-3 case cell price"> ${totalPrice/100} € </div>
-    
-    </div>
-    </div>
-    
-    `
-    
-    infosCart();
-    products.appendChild(panier);
-    
-    //envoi du prix total en session Storage
-    const infosPrice = {
-        prixTotal : totalPrice/100
-    }
-    let finalPrice = JSON.parse(sessionStorage.getItem('finalPrice'));
-    
-    if (finalPrice===null) {
-        finalPrice = [] 
-        
-    }
-    finalPrice.push(infosPrice);
-    localStorage.setItem('finalPrice', JSON.stringify(finalPrice));
-}
-
-
-
-
 // Formulaire //
 // récupération des id en local storage
 function getArrayId () {
@@ -185,6 +114,7 @@ function getArrayId () {
     return arrayId;
     
 }
+
 // vérification reggex du formulaire
 function isValid(value) {
     return /[A-Za-z\-\s]/.test(value);
@@ -194,7 +124,6 @@ function isValid(value) {
 var form = document.getElementById("form");
 form.addEventListener("submit", async  (e) => {
     e.preventDefault();
-    let lama = document.querySelector("#form");
     var firstName = form.elements.firstName.value;
     var lastName = form.elements.lastName.value;
     var address = form.elements.address.value;
