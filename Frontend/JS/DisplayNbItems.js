add79da4b8c150fbf3b9c4a537c59f56f6540e7e
Sraaday <60046022+Sraaday@users.noreply.github.com>
2020-10-08 17:08:23 +0200

    Refactor

---
+++ Frontend/JS/DisplayNbItems.js
@@ -0,0 +1,19 @@
+// Affichage nb produits panier
+function displayNbProduct() {
+    const items = JSON.parse(localStorage.getItem('cart'));
+    const NbItems = items == null ? 0 : items.length;
+    const anchorCart = document.querySelector('a[href="panier.html"]');
+    
+    let spanNbItems = document.querySelector('a[href="panier.html"] span');
+    if (spanNbItems == null) {
+        spanNbItems = document.createElement('span');
+        anchorCart.appendChild(spanNbItems);
+    }  
+    
+    spanNbItems.innerHTML = `&nbsp;(${NbItems})`;   
+    
+}
+
+displayNbProduct();
+
+// 
